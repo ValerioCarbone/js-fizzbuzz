f1abaae55b2509d9c24ffa234eb8f16673a74dc4
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-06 17:33:06 +0200

    CHANGE-README e pseudo-codice js

--- README.md
+++ README.md
@@ -9,3 +9,11 @@
 
 - Far in modo che il programma stampi per i multipli di 3 e di 5 la stringa fizzbuzz
     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 e di 3 danno come resto 0 stampino la parola buzz
+
+- Creare un div nel HTML per ogni m stampato dal ciclo for
+    - Richiamare un elemento dal DOM
+    - Creare all'interno di quell'elemento un numero elementi del DOM pari al numero di volte che si ripete il ciclo for
+    - Far si che per ogni elemento venga stampato nel HTML la stringa o il numero corretto
+
+- Dare uno stile ad ogni numero a seconda della divisibilità di m
+ 
--- js/app.js
+++ js/app.js
@@ -2,37 +2,42 @@
 //     - Creare un ciclo for che stampi i numeri in console da 1 a  0
 let containerDOMElement = document.querySelector('.container')
 
-for (let i = 0; i < 100; i++){
-// - Far in modo che il programma stampi per i multipli di 3 la stringa fizz
-//     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 3 danno come resto 0 stampino la parola fizz
+for (let i = 0; i < 100; i++) {
+    //  - Far in modo che il programma stampi per i multipli di 3 la stringa fizz
+    //     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 3 danno come resto 0 stampino la parola fizz
 
-// - Far in modo che il programma stampi per i multipli di 5 la stringa buzz
-//     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 danno come resto 0 stampino la parola buzz
+    // - Far in modo che il programma stampi per i multipli di 5 la stringa buzz
+    //     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 danno come resto 0 stampino la parola buzz
 
-// - Far in modo che il programma stampi per i multipli di 3 e di 5 la stringa fizzbuzz
-//     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 e di 3 danno come resto 0 stampino la parola buzz
+    // - Far in modo che il programma stampi per i multipli di 3 e di 5 la stringa fizzbuzz
+    //     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 e di 3 danno come resto 0 stampino la parola buzz
     let m = i + 1
     const n = m % 3
     const o = m % 5
     const p = n + o
-// - Richiamare un elemento dal DOM
-// - Creare all'interno di quell'elemento un numero elementi del DOM pari al numero di volte che si ripete il ciclo for
-// - Far si che per ogni elemento venga stampato nel HTML la stringa o il numero corretto
 
-    const contentDOMElement = document.createElement('div') 
-    containerDOMElement.append(contentDOMElement)   
+    // - Creare un div nel HTML per ogni m stampato dal ciclo for
+    //    - Richiamare un elemento dal DOM
+    //    - Creare all'interno di quell'elemento un numero elementi del DOM pari al numero di volte che si ripete il ciclo for
+    //    - Far si che per ogni elemento venga stampato nel HTML la stringa o il numero corretto
 
-    if (p===0){
-    console.log('FizzBuzz')
-    contentDOMElement.innerHTML = ('FizzBuzz')
-    contentDOMElement.classList.add('fizzbuzz')
+    // - Dare uno stile ad ogni numero a seconda della divisibilità di m
+
+
+    const contentDOMElement = document.createElement('div')
+    containerDOMElement.append(contentDOMElement)
+
+    if (p === 0) {
+        console.log('FizzBuzz')
+        contentDOMElement.innerHTML = ('FizzBuzz')
+        contentDOMElement.classList.add('fizzbuzz')
     }
-    else if (n===0){
+    else if (n === 0) {
         console.log('Fizz')
         contentDOMElement.innerHTML = ('Fizz')
         contentDOMElement.classList.add('fizz')
     }
-    else if (o===0){
+    else if (o === 0) {
         console.log('Buzz')
         contentDOMElement.innerHTML = ('Buzz')
         contentDOMElement.classList.add('buzz')
@@ -41,5 +46,5 @@ for (let i = 0; i < 100; i++){
         console.log(m)
         contentDOMElement.innerHTML = m
         contentDOMElement.classList.add('number')
-    }   
+    }
 }
