a9dfafa8100686fe884d283b595173ecdf939409
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-06 18:24:18 +0200

    CHANGE-Rename const ciclo+pulizia codice

--- js/app.js
+++ js/app.js
@@ -11,11 +11,7 @@ for (let i = 0; i < 100; i++) {
 
     // - Far in modo che il programma stampi per i multipli di 3 e di 5 la stringa fizzbuzz
     //     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 e di 3 danno come resto 0 stampino la parola buzz
-    let m = i + 1
-    const n = m % 3
-    const o = m % 5
-    const p = n + o
-
+    let counter = i + 1
     // - Creare un div nel HTML per ogni m stampato dal ciclo for
     //    - Richiamare un elemento dal DOM
     //    - Creare all'interno di quell'elemento un numero elementi del DOM pari al numero di volte che si ripete il ciclo for
@@ -27,24 +23,24 @@ for (let i = 0; i < 100; i++) {
     const contentDOMElement = document.createElement('div')
     containerDOMElement.append(contentDOMElement)
 
-    if (p === 0) {
+    if (((counter % 3) + (counter % 5)) === 0) {
         console.log('FizzBuzz')
         contentDOMElement.innerHTML = ('FizzBuzz')
         contentDOMElement.classList.add('fizzbuzz')
     }
-    else if (n === 0) {
+    else if ((counter % 3) === 0) {
         console.log('Fizz')
         contentDOMElement.innerHTML = ('Fizz')
         contentDOMElement.classList.add('fizz')
     }
-    else if (o === 0) {
+    else if ((counter % 5) === 0) {
         console.log('Buzz')
         contentDOMElement.innerHTML = ('Buzz')
         contentDOMElement.classList.add('buzz')
     }
     else {
-        console.log(m)
-        contentDOMElement.innerHTML = m
+        console.log(counter)
+        contentDOMElement.innerHTML = counter
         contentDOMElement.classList.add('number')
     }
 }
