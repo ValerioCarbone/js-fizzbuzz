0b56207350fdd6d41e2f9f0a1b2be078ff50c478
ValerioCarbone <valeriocarbone10@gmail.com>
2023-10-06 18:27:31 +0200

    CHANGE-Pulizia codice

--- js/app.js
+++ js/app.js
@@ -11,7 +11,7 @@ for (let i = 0; i < 100; i++) {
 
     // - Far in modo che il programma stampi per i multipli di 3 e di 5 la stringa fizzbuzz
     //     - Inserire all'interno del ciclo una condizione per cui i numeri che nella modale di 5 e di 3 danno come resto 0 stampino la parola buzz
-    let counter = i + 1
+    
     // - Creare un div nel HTML per ogni m stampato dal ciclo for
     //    - Richiamare un elemento dal DOM
     //    - Creare all'interno di quell'elemento un numero elementi del DOM pari al numero di volte che si ripete il ciclo for
@@ -19,6 +19,7 @@ for (let i = 0; i < 100; i++) {
 
     // - Dare uno stile ad ogni numero a seconda della divisibilità di m
 
+    let counter = i + 1
 
     const contentDOMElement = document.createElement('div')
     containerDOMElement.append(contentDOMElement)
